0f8f0b343abad8c5d48f1f32008d26421b93d746
Weigang Geng <gengwg@gmail.com>
2017-10-06 23:54:46 -0700

    check limits for a running process

---
+++ limits.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# http://blackbird.si/checking-limits-of-a-linux-process-ulimit/
+# check limits for a running process
+# Usage: ./limits.sh <process name>
+
+if [ "$#" -ne "1" ]; then
+   echo ""
+   echo -e "\033[01;32mLimit checker\033[00m"
+   echo -e "\033[01;37mUsage:\033[01;33m $0 process_name\033[00m"
+   echo ""
+   exit 0
+fi
+
+return-limits(){
+   for process in $@; do
+      process_pids=`ps -C $process -o pid --no-headers | cut -d " " -f 2`
+
+      if [ -z $@ ]; then
+         echo "[no $process running]"
+      else
+         for pid in $process_pids; do
+            echo "[$process #$pid -- limits]"
+            cat /proc/$pid/limits
+      done
+      fi
+   done
+}
+
+return-limits $1
