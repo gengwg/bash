9b8b392ddab0f08c7e7d70b3eeaf6c7f06f332e6
Weigang Geng <gengwg@gmail.com>
2017-11-28 22:57:30 -0800

    add eadme

--- youtube-mplayer.sh
+++ youtube-mplayer.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Play youtube video using mplayer in terminal.
+# Useful for playing music from youtube.
+#
 # Usage:
 #     ./youtube-mplayer.sh https://www.youtube.com/watch?v=dyFYJclrSAc
 youtube-dl -q -o- "$*" | mplayer -novideo -af scaletempo  -softvol -softvol-max 400  -cache 8192  -
